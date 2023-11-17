bc03d55be7e214d4c121e728145ab1899e174511
Praveen Singh <singhpravu@gmail.com>
2023-11-17 21:54:58 +0530

    Day-1

---
+++ Conditions.java
@@ -0,0 +1,129 @@
+// Print the largest of 2_
+// public class Conditions {
+// public static void main(String[] args) {
+// int A = 1;
+// int B = 3;
+// if (A > B) {
+// System.out.println("A is Largest");
+// } else {
+// System.out.println("B is Largest");
+// }
+// }
+// }
+
+// // Print if a number is Odd or Even_
+// import java.util.*;
+
+// public class Conditions {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int number = sc.nextInt();
+// if (number % 2 == 0) {
+// System.out.println("It is a Even number");
+// } else {
+// System.out.println("It is a Odd number");
+// }
+
+// }
+// }
+
+// // Income Tax Calculator_
+// import java.util.*;
+
+// public class Conditions {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// double income = sc.nextDouble();
+// if (income < 500000) {
+// System.out.println(income);
+// } else if (income > 500000 || income < 1000000) {
+// System.out.println(income + (income * 20 / 100));
+// } else {
+// System.out.println(income + (income * 30 / 100));
+// }
+// }
+// }
+
+// // Print the largest of 3_
+// public class Conditions {
+
+// public static void main(String[] args) {
+// int A = 9, B = 8, C = 6;
+// if (A > B && A > C) {
+// System.out.println("A is the greatest");
+// } else if (B > C) {
+// System.out.println("B is the greatest");
+// } else {
+// System.out.println(" C is the greatest");
+// }
+// }
+// }
+
+// // Check if a student will Pass or Fail_
+// public class Conditions {
+
+// public static void main(String[] args) {
+// int marks = 4;
+// String result = (marks >= 33) ? "Pass" : "Fail";
+// System.out.println(result);
+// }
+// }
+
+// // Switch statement_
+// public class Conditions {
+// public static void main(String[] args) {
+// int number = 6;
+// switch (number) {
+// case 1:
+// System.out.println("Samosa");
+// break;
+// case 2:
+// System.out.println("Burger");
+// break;
+// case 3:
+// System.out.println("Mango Shake");
+// break;
+// default:
+// System.out.println("We are dreaming");
+// break;
+// }
+// }
+// }
+
+// // Calculator_
+
+// public class Conditions {
+
+// public static void main(String[] args) {
+// int a = 30;
+// int b = 20;
+// char operator = '%';
+// switch (operator) {
+// case '+':
+// System.out.println(a + b);
+
+// break;
+// case '-':
+// System.out.println(a - b);
+
+// break;
+// case '*':
+// System.out.println(a * b);
+
+// break;
+// case '/':
+// System.out.println(a / b);
+
+// break;
+// case '%':
+// System.out.println(a % b);
+
+// break;
+
+// default:
+// System.out.println("Invalid operator");
+// break;
+// }
+// }
+// }
---
+++ Variables.java
@@ -0,0 +1,65 @@
+// // Print Pattern_
+// public class Variables {
+// public static void main(String[] args) {
+// System.out.println("****");
+// System.out.println("***");
+// System.out.println("**");
+// System.out.println("*");
+// }
+// }
+
+// // Variables in Java_
+// public class Variables {
+// public static void main(String[] args) {
+// int a = 10, b = 5;
+// int eq = 2 * (a + b);
+// System.out.println(eq);
+// }
+// }
+
+// // Sum of a & b_
+// public class Variables {
+// public static void main(String[] args) {
+// int a = 10;
+// int b = 20;
+// int sum = a + b;
+// System.out.println(sum);
+// }
+// }
+
+// // Sum of a & b_
+// import java.util.*;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// int sum = a + b;
+// System.out.println(sum);
+// }
+
+// }
+
+// // Product of a & b_
+// import java.util.*;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// int product = a * b;
+// System.out.println(product);
+// }
+
+// }
+
+// // Area of a Circle_
+// import java.util.*;
+// public class Variables {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// float radious = sc.nextFloat();
+// float area = (2 * 3.14f * (radious * radious));
+// System.out.println(area);
+// }
+// }
