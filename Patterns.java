55d250dbb5e989b88cc6947217ee0073eb0b8332
Praveen Singh <singhpravu@gmail.com>
2023-11-19 15:27:04 +0530

    Patterns

--- Patterns.java
+++ Patterns.java
@@ -24,20 +24,32 @@
 // }
 // }
 
-// Print Cha
-public class Patterns {
-    public static void main(String[] args) {
-        int n = 4;
-        char ch = 'A';
+// // Print Character pattern
+// public class Patterns {
+// public static void main(String[] args) {
+// int n = 4;
+// char ch = 'A';
+// // outer loop
+// for (int i = 1; i <= n; i++) {
+// // inner loop
+// for (int chars = 1; chars <= i; chars++) {
+// System.out.print(ch);
+// ch++;
+// }
+// System.out.println();
+// }
+// }
+// }
 
-        // outer loop
-        for (int i = 1; i <= n; i++) {
-            // inner loop
-            for (int chars = 1; chars <= i; chars++) {
-                System.out.print(ch);
-                ch++;
-            }
-            System.out.println();
-        }
-    }
-}
+// // Print half piramid Pattern
+// public class Variables {
+// public static void main(String[] args) {
+// int n = 4;
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print(j);
+// }
+// System.out.println();
+// }
+// }
+// }
