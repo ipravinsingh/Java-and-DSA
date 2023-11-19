a42e43d28320c5bf5c73aff6be6c2b58f50f37a3
Praveen Singh <singhpravu@gmail.com>
2023-11-19 15:22:21 +0530

    Patterns

---
+++ Patterns.java
@@ -0,0 +1,43 @@
+// Print Star Pattern
+// public class Patterns {
+// public static void main(String[] args) {
+// int n = 4;
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= i; j++) {
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+// }
+
+// // Print Inverted Star Pattern
+// public class Patterns {
+// public static void main(String[] args) {
+// int n = 4;
+// for (int i = 1; i <= n; i++) {
+// for (int j = 1; j <= n - i + 1; j++) {
+// System.out.print("*");
+// }
+// System.out.println();
+// }
+// }
+// }
+
+// Print Cha
+public class Patterns {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int chars = 1; chars <= i; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
