b305c9c5e6514f28fa3e802959c952d3d8653a22
Praveen Singh <singhpravu@gmail.com>
2023-11-27 18:42:13 +0530

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,14 @@
+// public class Arrays {
+
+//     public static void main(String[] args) {
+
+//     }
+// }
+
+// 2
 public class Arrays {
 
     public static void main(String[] args) {
-        
+
     }
 }
