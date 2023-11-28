5c4ef4b9b1909e5035c02fc744fa61a1bc3140df
Praveen Singh <singhpravu@gmail.com>
2023-11-28 17:35:14 +0530

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -1,14 +1,163 @@
+// // Print marks of an array_
+// import java.util.Scanner;
 // public class Arrays {
-
-//     public static void main(String[] args) {
-
-//     }
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int marks[] = new int[50];
+// marks[0] = sc.nextInt();
+// marks[1] = sc.nextInt();
+// marks[2] = sc.nextInt();
+// System.out.println("Phys : " + marks[0]);
+// System.out.println("Chem : " + marks[1]);
+// System.out.println("Maths : " + marks[2]);
+// }
 // }
 
-// 2
-public class Arrays {
+// // Print array of marks by adding one_
+// public class Arrays {
+// public static void marksIncreaseOne(int marks[]) {
+// for (int i = 0; i < marks.length; i++) {
+// marks[i] = marks[i] + 1;
+// }
+// }
+// public static void main(String[] args) {
+// int marks[] = { 97, 98, 99 };
+// marksIncreaseOne(marks);
+// for (int i = 0; i < marks.length; i++) {
+// System.out.println(marks[i]);
+// }
+// }
+// }
 
-    public static void main(String[] args) {
+// // Find the index of element in a given array
+// public class Arrays {
+// public static int linearSearch(int arr[], int key) {
+// for (int i = 0; i < arr.length; i++) {
+// if (arr[i] == key) {
+// return i;
+// }
+// }
+// return -1;
+// }
+// public static void main(String[] args) {
+// int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+// int key = 20;
+// int index = linearSearch(arr, key);
+// if (index == -1) {
+// System.out.println("Not Found");
+// } else {
+// System.out.println("Key is at index : " + index);
+// }
+// }
+// }
 
-    }
-}
+// // Find the largest & smallest number in a given array
+// public class Arrays {
+// public static int largestNumber(int arr[]) {
+// int largest = Integer.MIN_VALUE;
+// int smallest = Integer.MAX_VALUE;
+// for (int i = 0; i < arr.length - 1; i++) {
+// if (largest < arr[i]) {
+// largest = arr[i];
+// }
+// if (smallest > arr[i]) {
+// smallest = arr[i];
+// }
+// }
+// System.out.println("Smallest value is : " + smallest);
+// return largest;
+// }
+// public static void main(String[] args) {
+// int arr[] = { 1, 2, 6, 13, 5 };
+// System.out.println("Largest value is : " + largestNumber(arr));
+// }
+// }
+
+// // Find the index of element in a given array byBinary search_
+// public class Arrays {
+// public static int binarySearch(int arr[], int key) {
+// int start = 0;
+// int end = arr.length - 1;
+// while (start <= end) {
+// int mid = (start + end) / 2;
+// if (arr[mid] == key) {
+// return mid;
+// }
+// if (arr[mid] < key) {
+// start = mid + 1;
+// } else {
+// end = mid - 1;
+// }
+// }
+// return -1;
+// }
+// public static void main(String[] args) {
+// int arr[] = { 2, 4, 6, 8, 10, 12 };
+// int key = 100;
+// System.out.println(binarySearch(arr, key));
+// }
+// }
+
+// // Reverse an Array
+// public class Arrays {
+// public static void reverseArray(int arr[]) {
+// int start = 0;
+// int end = arr.length - 1;
+// while (start < end) {
+// int temp;
+// temp = arr[end];
+// arr[end] = arr[start];
+// arr[start] = temp;
+// start++;
+// end--;
+// }
+// }
+// public static void main(String[] args) {
+// int arr[] = { 2, 4, 6, 8, 10 };
+// reverseArray(arr);
+// for (int i = 0; i < arr.length; i++) {
+// System.out.print(arr[i] + " ");
+// }
+// }
+// }
+
+// // Pairs in an Array
+// public class Arrays {
+// public static void pairs(int arr[]) {
+// int totalPair = 0;
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = i+1; j < arr.length; j++) {
+// System.out.print("(" + arr[i] + "," + arr[j] + ")");
+// totalPair++;
+// }
+// System.out.println();
+// }
+// System.out.println("Total Pairs are : " + totalPair);
+// }
+// public static void main(String[] args) {
+// int arr[] = { 2, 4, 6, 8, 10 };
+// pairs(arr);
+// }
+// }
+
+// // Print Subarrays
+// public class Arrays {
+// public static void printSubarrays(int arr[]) {
+// int ts = 0;
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = i; j < arr.length; j++) {
+// for (int k = i; k <= j; k++) {
+// System.out.print(arr[k] + " ");
+// }
+// ts++;
+// System.out.println();
+// }
+// System.out.println();
+// }
+// System.out.println("Total : " + ts);
+// }
+// public static void main(String[] args) {
+// int arr[] = { 2, 4, 6, 8, 10 };
+// printSubarrays(arr);
+// }
+// }
